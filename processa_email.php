a69d46cbb51fc8ee76bd7fbd46f4a9a584e1ed18
Alexandre Gonçalves Teixeira <a92teixeira@gmail.com>
2021-03-23 09:13:30 -0300

    Send mail included with success

--- processa_email.php
+++ processa_email.php
@@ -1,5 +1,14 @@
 <?php
 
+require "lib/PHPMailer/Exception.php";
+require "lib/PHPMailer/OAuth.php";
+require "lib/PHPMailer/PHPMailer.php";
+require "lib/PHPMailer/POP3.php";
+require "lib/PHPMailer/SMTP.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 class Mensagem{
    public $para = null;
    public $assunto = null;
@@ -27,11 +36,47 @@ $mensagem->__set('para', $_POST['para']);
 $mensagem->__set('assunto', $_POST['assunto']);
 $mensagem->__set('mensagem', $_POST['mensagem']);
 
-if($mensagem->validar_mensagem()){
-   echo 'Mensagem válida';
-}else{
-   echo "Mensagem inválida";
+if(!$mensagem->validar_mensagem()){
+   echo 'A mensagem não é válida, preencha todos os campos do formulário';
+   die();
 }
 
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'a92teixeiradev@gmail.com';                     //SMTP username
+    $mail->Password   = 'a92devtest';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('a92teixeiradev@gmail.com', 'Mailer Test');
+    $mail->addAddress('a92teixeiradev@gmail.com', 'Iha User');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Teste bem sucedido';
+    $mail->Body    = 'Esse email contém um HTML <b>básico</b>';
+    $mail->AltBody = 'Esse email não contém HTML';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "A mensagem não pôde ser enviada, codigo: {$mail->ErrorInfo}";
+}
 
 ?>
