b4e1462932730c17423c992fbf17532db13f07ca
Alexandre Gonçalves Teixeira <a92teixeira@gmail.com>
2021-03-23 22:37:47 -0300

    App Send Mail working in 23/03/21

--- processa_email.php
+++ processa_email.php
@@ -1,5 +1,11 @@
-<?php
+<?php 
 
+//por segurança proteger o arquivo fora do diretorio publico
+require '../../app_send_mail/processa_email.php';
+
+/****REQUIRE*****/
+
+/*
 require "lib/PHPMailer/Exception.php";
 require "lib/PHPMailer/OAuth.php";
 require "lib/PHPMailer/PHPMailer.php";
@@ -10,9 +16,11 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 class Mensagem{
-   public $para = null;
-   public $assunto = null;
-   public $mensagem = null;
+   private $para = null;
+   private $assunto = null;
+   private $mensagem = null;
+
+   public $status = array('cod_status' => null, 'desc_status' => null);
 
    public function __get($atributo){
       return $this->$atributo;
@@ -38,7 +46,8 @@ $mensagem->__set('mensagem', $_POST['mensagem']);
 
 if(!$mensagem->validar_mensagem()){
    echo 'A mensagem não é válida, preencha todos os campos do formulário';
-   die();
+   //erro=1 : algum campo obrigatório da aplicação nao foi preenchido
+   header("Location: index.php?erro=1");
 }
 
 //Instantiation and passing `true` enables exceptions
@@ -51,13 +60,17 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'a92teixeiradev@gmail.com';                     //SMTP username
-    $mail->Password   = 'a92devtest';                               //SMTP password
+    $mail->Password   = '*****';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
+    //encode to utf-8
+    $mail->CharSet = "UTF-8"; 
+
     //Recipients
-    $mail->setFrom('a92teixeiradev@gmail.com', 'Mailer Test');
-    $mail->addAddress('a92teixeiradev@gmail.com', 'Iha User');     //Add a recipient
+    $mail->setFrom('a92teixeiradev@gmail.com', 'SourcesDIY Company');
+    //$mail->setFrom();
+    $mail->addAddress($mensagem->__get('para'), 'Cliente #0974');     //Add a recipient
     //$mail->addAddress('ellen@example.com');               //Name is optional
     //$mail->addReplyTo('info@example.com', 'Information');
     //$mail->addCC('cc@example.com');
@@ -69,14 +82,61 @@ try {
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Teste bem sucedido';
-    $mail->Body    = 'Esse email contém um HTML <b>básico</b>';
-    $mail->AltBody = 'Esse email não contém HTML';
+    $mail->Subject = $mensagem->__get('assunto');
+    $mail->Body    = $mensagem->__get('mensagem');
+    $mail->AltBody = 'É necessário utilizar um client que suporte HTML para utilizar este recurso';
 
     $mail->send();
-    echo 'Message has been sent';
+
+    $mensagem->status['cod_status'] = 1;
+    $mensagem->status['desc_status'] = "E-mail enviado com sucesso";
+
 } catch (Exception $e) {
-    echo "A mensagem não pôde ser enviada, codigo: {$mail->ErrorInfo}";
+   $mensagem->status['cod_status'] = 0;
+   $mensagem->status['desc_status'] = "Não foi possível enviar este email, tente novamente mais tarde. Detalhes do erro:" . $mail->ErrorInfo;
 }
+/****END REQUIRE*****/
 
 ?>
+
+<!--
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>App Send Mail</title>
+
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+<div class='container'>
+   <div class="py-3 text-center">
+      <img src="img/logo.png" alt="">
+      <h2>Send Mail</h2>
+      <p class="lead">Seu app de envio de emails particular</p>
+   </div>
+
+   <div class="row">
+      <div class="col-md-12">
+      < ?php if($mensagem->status['cod_status'] == 1){ ?>
+         <div class="container">
+            <h1 class="display-4 text-success">Success</h1>
+            <p> < ? = $mensagem->status['desc_status']; ?> </p>
+            <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+         </div>
+         < ?php } ?>
+
+         < ?php if($mensagem->status['cod_status'] == 0){ ?>
+         <div class="container">
+            <h1 class="display-4 text-danger">Ops!</h1>
+            <p> < ?= $mensagem->status['desc_status']; ?> </p>
+            <a href="index.php" class="btn btn-danger btn-lg mt-5 text-white">Voltar</a>
+         </div>
+         < ?php } ?>
+
+      </div>
+   </div>
+
+</div> <--FIM CONTAINER-->
